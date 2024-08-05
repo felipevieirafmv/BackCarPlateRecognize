63233f878c1879a5e155c3db9ee68f2023a67d72
unknown <Felipe de Mello Vieira>
2024-08-05 10:24:13 -0300

    Starting JWT

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -1,5 +1,9 @@
 import Funcionario from '../model/funcionario.js';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv-safe';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
 
 export default class LoginController {
 
@@ -27,7 +31,11 @@ export default class LoginController {
         return res.status(422).json({ message: "EDV e/ou senha inválidos" });
       }
 
-      return res.status(200).send({ message: "Usuário logado com sucesso" });
+      const token = jwt.sign({ id: user.ID }, process.env.SECRET, {
+        expiresIn: 300 // expires in 5min
+      });
+
+      return res.status(200).send({ auth: true, token: token });
     } catch (error) {
       console.error("Erro ao fazer login: ", error);
       return res.status(500).send({ message: "Ocorreu um erro interno", data: error.message });
